99eec153442a18ab1069849433247fdb9ccec7ca
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2023-08-18 17:52:32 -0300

    feat: add row in table on click submit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const newContact = document.getElementById("btn-new-contact");
+const btnNewContact = document.getElementById("btn-new-contact");
 const form = document.getElementById("main-form");
+const table = document.getElementById("main-table");
+const tableRowDefault = document.getElementById("tr-default");
 
 function hideElements(...elements) {
     elements.forEach((el) => el.classList.add("hide"));
@@ -7,6 +9,37 @@ function hideElements(...elements) {
 
 function showElements(...elements) {
     elements.forEach((el) => el.classList.remove("hide"));
+}
+
+function newContact() {
+    let contact = {};
+
+    contact.name = form.querySelector("#tel-name").value;
+    contact.tel = `(${form.querySelector("#tel-ddd").value}) `;
+    contact.tel += form.querySelector("#tel-num").value;
+    contact.iconLetter = contact.name[0].toUpperCase();
+    
+    return contact;
+}
+
+function addContactRow(contact) {
+    const contactIcon = document.createElement("td");
+    contactIcon.innerHTML = `<p class="circle">${contact.iconLetter}</p>`
+
+    const contactName = document.createElement("td");
+    contactName.innerText = contact.name;
+
+    const contactTel = document.createElement("td");
+    contactTel.innerText = contact.tel;
+
+    const row = document.createElement("tr");
+    row.className = "container-row";
+
+    row.appendChild(contactIcon);
+    row.appendChild(contactName);
+    row.appendChild(contactTel);
+
+    table.querySelector("tbody").appendChild(row);
 }
 
 function onNewContactClick(event) {
@@ -18,7 +51,10 @@ function onNewContactClick(event) {
 function onFormSubmit(event) {
     event.preventDefault();
 
-    hideElements(form);
+    contact = newContact();
+    addContactRow(contact);
+
+    hideElements(form, tableRowDefault);
 }
 
 function onFormReset(event) {
@@ -27,6 +63,6 @@ function onFormReset(event) {
     hideElements(form);
 }
 
-newContact.addEventListener("click", (e) => onNewContactClick(e));
+btnNewContact.addEventListener("click", (e) => onNewContactClick(e));
 form.addEventListener("submit", (e) => onFormSubmit(e));
 form.addEventListener("reset", (e) => onFormReset(e));
